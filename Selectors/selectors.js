6e0adf3061aacfa342e1cbe05d8ba881e65a4890
Burhan <ahmetilov20@gmail.com>
2023-08-26 16:43:52 +0200

    first commit

---
+++ Selectors/selectors.js
@@ -0,0 +1,19 @@
+function func1(){
+    //$(element name).action()
+    //document.getElementByTagName(element name)
+
+    // $("div").css("background-color", "orange");
+    // $("#p1").css("font-style", "italic");
+    // $(".para").css("font-style", "italic");
+
+    // $("#div1, #div3").css("background-color", "green");
+    // $("#div1, li").css("background-color", "purple");
+
+    // $("div > p").fadeToggle();
+    // $("div p").fadeToggle();
+    // $("p:first").fadeToggle();
+    // $("p:second").fadeToggle();
+
+    // $("li:even").fadeToggle(); //even indexes are gone, hence odd numbers in this case.
+    // $("li:odd").fadeToggle();
+}
